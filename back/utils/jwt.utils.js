5de8da238e3b6cb2e5a09159eb18f002b00ff9e9
unknown <illshaad.budureea@avec.fr>
2021-12-10 15:01:35 +0100

    setup jwt routing express

--- back/utils/jwt.utils.js
+++ back/utils/jwt.utils.js
@@ -1,18 +1,31 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
-const JWT_SECRET = '8181f8sfsfsfsfzezfzef1z01f8z1f8dsdf1sf51sf1sffzf8ef1ze8fza' ;
+const JWT_SECRET = process.env.JWT_SECRET;
+
 const generateTokenUser = (userData) => {
-    return jwt.sign({
-        id : userData.id,
-        isAdmin : userData.isAdmin,
+  return jwt.sign(
+    {
+      userId: userData._id,
+      isAdmin: userData.isAdmin,
     },
-    JWT_SECRET,{
-      expiresIn : '1h'
-    })
-}
+    JWT_SECRET,
+    {
+      expiresIn: "3h",
+    }
+  );
+};
 
+const parseAuthorizaion = (authorization) =>
+  authorization !== null && authorization.split(" ")[1];
 
+const getUser = (authorization) => {
+  try {
+    const token = parseAuthorizaion(authorization);
+    const jwtToken = jwt.verify(token, JWT_SECRET);
+    if (jwtToken !== null) return jwtToken.userId;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
-module.exports = { generateTokenUser  };
-
-//Mettre id a un utilisateur crée
+module.exports = { generateTokenUser, getUser };
